b4eac3a15afa0f26ca2bcd067c0728673144f0b0
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-01-27 17:17:50 +0900

    new: support tailwindcss.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <img alt="logo" className="logo" src={electronLogo} />
       <div className="creator">Powered by electron-vite</div>
       <div className="text">
